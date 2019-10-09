d522d92f3dd5d3b31950bb54ddc714dc38b21929
Kieran Healy <kjhealy@gmail.com>
2019-10-08 20:28:29 -0400

    Add the beginnings of a Vignette

--- R/onLoad.r
+++ R/onLoad.r
@@ -3,10 +3,10 @@
 ##' A message shown when attaching the package, to make clear what the
 ##'     main data objects are.
 ##' @title .onAttach
-##' @param libname 
-##' @param pkgname 
+##' @param libname gssr
+##' @param pkgname gssr
 ##' @return Message
 ##' @author Kieran Healy
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Package loaded. To attach the GSS data, type data(gss_all) at the console. For the codebook, type data(gss_doc). The gss_all and gss_doc objects will then be available to use.")
+  packageStartupMessage("Package loaded. To attach the GSS data, type data(gss_all) at the console.\nFor the codebook, type data(gss_doc). The gss_all and gss_doc objects will then be available to use.")
 }
