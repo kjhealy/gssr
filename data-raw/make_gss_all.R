d72a3a3989825fdececbeb9054d7a4759e64974a
Kieran Healy <kjhealy@gmail.com>
2025-10-28 07:07:18 -0400

    Update to GSS 1972-2024 R2, Oct 2025

--- data-raw/make_gss_all.R
+++ data-raw/make_gss_all.R
@@ -46,25 +46,33 @@ library(here)
 # attr(tmp$wlthwhts, "label")
 # attr(tmp$wlthwhts, "labels")
 
-
 ## Read the Stata file
-gss_all <- read_stata(here::here("data-raw", "norc", "gss7224_r1.dta"))
+gss_all <- read_stata(here::here("data-raw", "norc", "gss7224_r2.dta"))
 
 ## Small version for vignettes
 cont_vars <- c("year", "id", "ballot", "age")
 
-cat_vars <- c("race", "sex", "degree", "padeg",
-              "madeg", "relig", "polviews",
-              "fefam")
+cat_vars <- c(
+  "race",
+  "sex",
+  "degree",
+  "padeg",
+  "madeg",
+  "relig",
+  "polviews",
+  "fefam"
+)
 
-wt_vars <- c("vpsu",
-             "vstrat",
-             "oversamp",
-             "formwt",              # weight to deal with experimental randomization
-             "wtssall",             # weight variable (1972-2018)
-             "wtssps",              # post-stratification weights (1972-2024)
-             "sampcode",            # sampling error code
-             "sample")              # sampling frame and method
+wt_vars <- c(
+  "vpsu",
+  "vstrat",
+  "oversamp",
+  "formwt", # weight to deal with experimental randomization
+  "wtssall", # weight variable (1972-2018)
+  "wtssps", # post-stratification weights (1972-2024)
+  "sampcode", # sampling error code
+  "sample"
+) # sampling frame and method
 
 sub_vars <- c(cont_vars, cat_vars, wt_vars)
 
@@ -76,5 +84,3 @@ gss_sub <- labelled::copy_labels(gss_all, gss_sub)
 ## Save out
 usethis::use_data(gss_all, overwrite = TRUE, compress = "xz")
 usethis::use_data(gss_sub, overwrite = TRUE, compress = "xz")
-
-
